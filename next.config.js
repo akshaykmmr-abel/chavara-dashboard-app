c76fd462e3b9300ffdc2fe91c6007a29d08c0baa
akshaykmmr-abel <akshay.kumar@abeltechsoft.com>
2025-12-26 15:09:24 +0530

    Configure for static export

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone',
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
   env: {
     BACKEND_API_URL: process.env.BACKEND_API_URL || 'https://api.chavaramatrimony.com',
     BACKEND_AUTH_VERIFY_URL: process.env.BACKEND_AUTH_VERIFY_URL || 'https://api.chavaramatrimony.com/auth/verify',
